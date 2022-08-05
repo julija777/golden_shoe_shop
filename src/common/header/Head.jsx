249d15266d50d39152b61a35d4daffc92fa6cbc2
Julia777 <s.julija@me.com>
2022-08-06 00:37:37 +0100

    header started, font awesome added

---
+++ src/common/header/Head.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Head = () => {
+  return (
+    <>
+    <section
+        className="head">
+          <div className="container d_flex">
+            <div className="left-row">
+                <i className="fa fa-phone"></i>
+                <label htmlFor='phone'>+447980172233</label>
+                <i className="fa fa-envelope"></i>
+                <label htmlFor='email'>GoldenShoe@shop.react</label>
+            </div>
+            </div>  
+        </section>
+    </>
+  );
+}
+
+export default Head;
