0c65c528c8a4e36874e86dcafaca42794bab65e8
Julia777 <s.julija@me.com>
2022-08-06 17:03:30 +0100

    NavBar set and rendered

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import React from 'react';
 import './App.css';
 import Header from './common/header/Header';
+// eslint-disable-next-line
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-     <Header/>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/"/>
+          {/* <About /> */}
+          <Route />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
