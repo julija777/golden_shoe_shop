2cd2611639505cdd76a4b859d541d7c7bccd8f7d
Julia777 <s.julija@me.com>
2022-08-06 18:13:37 +0100

    Search component added with inputs and fontawesome icons in it, links to cart added

--- src/common/header/Search.jsx
+++ src/common/header/Search.jsx
@@ -1,10 +1,35 @@
 import React from "react";
+import logo from "./assets/logo.svg";
+import { Link } from "react-router-dom";
+
 
 const Search = () => {
   return (
-    <div>
-      <h1>Search</h1>
-    </div>
+    <>
+    <section className="search">
+      <div className="container c_flex">
+        <div className="logo width">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="search-box">
+          <i className="fa fa-search"></i>
+          <input type="text" placeholder="Search" />
+          <span>All Categories</span>
+          </div>
+
+        <div className="icon f_flex width">
+          <i className="fa fa-user icon_circle"></i>
+          <div className="cart">
+            <Link to="/cart">
+            <i className="fa fa-shopping-bag icon_circle"></i>
+            </Link>
+<span>0</span>
+        </div>
+        </div>
+      </div>
+
+        </section>
+    </>
   );
 }
 
