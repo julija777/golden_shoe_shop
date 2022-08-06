7e1fa99ae24b15fa22ddbcf27ecb5ce374bcb38e
Julia777 <s.julija@me.com>
2022-08-06 15:49:49 +0100

    header created and styled

--- src/common/header/Head.jsx
+++ src/common/header/Head.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "./Header.css";
+
 
 const Head = () => {
   return (
@@ -10,8 +12,16 @@ const Head = () => {
                 <i className="fa fa-phone"></i>
                 <label htmlFor='phone'>+447980172233</label>
                 <i className="fa fa-envelope"></i>
-                <label htmlFor='email'>GoldenShoe@shop.react</label>
+                <label>GoldenShoe@shop.react</label>
             </div>
+            <div className="right row RText">
+              <label>FAQ</label>
+              <label>Need Help</label>
+              <span>🇬🇧</span>
+              <label>EN</label>
+              <span>🇺🇲</span>
+              <label>USD</label>
+              </div>
             </div>  
         </section>
     </>
--- src/common/header/Navbar.jsx
+++ src/common/header/Navbar.jsx
@@ -2,9 +2,17 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <>
+      <header className="header">
+        <div className="container d_flex">
+        <div className="container d_flex">
+          <span className="fa-solid fa-border=all">
+
+          </span>
+          
+          </div></div>
+      </header>
+    </>
   );
 }
 
